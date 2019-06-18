a14a6f9ec7f9508ffbb61cf10f1f2705d5dbeb4f
wangyg <wangyg@gate31.fast>
2019-06-18 20:38:16 +0800

    modified:   pipe/convert_data.py

--- pipe/convert_data.py
+++ pipe/convert_data.py
@@ -5,9 +5,10 @@ data_path   = '/data31/2019a-053-SC/SDSS_N_2a/20190527/'
 data_file_tmp = 'SDSS_N_2a_arcdrfit'
 output_path = '/home/wangyg/tmp/' + data_file_tmp + '%04d-%04d.h5'
 data_file   = data_file_tmp + '-M%02d_W_%04d.fits'
-dec0 = 25.65294
+#dec0 = 25.65294
+dec0 = 26.15294 
 beam_list   = np.arange(19) + 1
-block_list  = np.arange(2) + 1 + 10
+block_list  = np.arange(10) + 1 + 10
 
 dc.convert_to_tl(data_path, data_file, 
         output_path%(block_list[0], block_list[-1]),
